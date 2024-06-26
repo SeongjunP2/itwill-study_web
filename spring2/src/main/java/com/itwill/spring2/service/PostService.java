d75031455957dfdfdac35e0f8c3ab6b2aacb7517
SeongjunP2 <guppy0217@gmail.com>
2024-06-26 11:17:01 +0900

    update PostService(deleteByPostId)

--- spring2/src/main/java/com/itwill/spring2/service/PostService.java
+++ spring2/src/main/java/com/itwill/spring2/service/PostService.java
@@ -10,6 +10,7 @@ import com.itwill.spring2.dto.PostCreateDto;
 import com.itwill.spring2.dto.PostListDto;
 import com.itwill.spring2.dto.PostSearchDto;
 import com.itwill.spring2.dto.PostUpdateDto;
+import com.itwill.spring2.repository.CommentDao;
 import com.itwill.spring2.repository.Post;
 import com.itwill.spring2.repository.PostDao;
 
@@ -30,6 +31,8 @@ public class PostService {
 //	public PostService(PostDao postDao) {
 //		this.postDao = postDao;
 //	}
+	
+    private final CommentDao commentDao; // 생성자에 의한 의존성 주입
 	
 	public List<PostListDto> read() {
 		log.debug("read()");
@@ -61,15 +64,20 @@ public class PostService {
 		return result;
 	}
 	
-	public int delete(int id) {
-		log.debug("delete(id={})", id);
-		
-		// 리포지토리 컴포넌트의 메서드를 호출해서 delete 쿼리를 실행.
-		int result = postDao.deletePost(id);
-		log.debug("delete 결과 = {}", result);
-		
-		return result;
-	}
+    public int delete(int id) {
+        log.debug("delete(id={})", id);
+        
+        // 리포지토리 컴포넌트의 메서드를 호출해서 delete 쿼리를 실행.
+        // (1) 포스트에 달려 있는 모든 댓글들을 삭제:
+        int rows = commentDao.deleteByPostId(id);
+        log.debug("삭제된 댓글 개수 = {}", rows);
+        
+        // (2) 포스트를 삭제:
+        int result = postDao.deletePost(id);
+        log.debug("delete 결과 = {}", result);
+        
+        return result;
+    }
 	
 	public int update(PostUpdateDto dto) {
 		log.debug("update(id={})", dto);
